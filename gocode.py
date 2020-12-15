7b904d8d9c65bea8991c24fa59bf99e10c6fad0a
Isaiah08-D <66925301+Isaiah08-D@users.noreply.github.com>
2020-12-15 10:09:20 -0500

    Update gocode.py

--- gocode.py
+++ gocode.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, flash, url_for
 from replit import db
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+import forms.py
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template("index.html", db=db)
+
+@app.route('/code')
+def code():
+  
