1da5b6607d4a8abb7946b89a0e177bbfc2c6c2ff
Isaiah08-D <66925301+Isaiah08-D@users.noreply.github.com>
2020-12-15 10:15:52 -0500

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+
+
+    example = RadioField('Label', choices=['value','value_two'])
