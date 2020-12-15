ab13d16e7eda1d4208cd96ff3eab4ed7d16cf0ce
Isaiah08-D <66925301+Isaiah08-D@users.noreply.github.com>
2020-12-15 17:56:29 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 
 class chose_language:
-    language = RadioField('Chose a language:', choices=[('Python','A dynamic language that can do both simple and very complex things.'),('HTML, CSS, JS','The language that makes up the web.')], validators=[DataRequired()])
-    submit = SubmitField('Code!')
+	language = RadioField('Chose a language:', choices=[('Python','A dynamic language that can do both simple and very complex things.'),('HTML, CSS, JS','The language that makes up the web.')], validators=[DataRequired()])
+	submit = SubmitField('Code!')
