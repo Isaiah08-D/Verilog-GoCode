a222f40571ee5d6d0b1cf47892af48de8e7379dc
isaiah08 <isaiahgrisha@gmail.com>
2020-12-18 22:11:21 +0000

    Added a few things

--- gocode.py
+++ gocode.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, session, redirect, flash, url_for
 from replit import db
-import forms.py as forms
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'VLXG5dASre6k91gvFGAh'
+bootstrap = Bootstrap(app)
 
+class chose_language(FlaskForm):
+	language = RadioField('Chose a language:', choices=[('A dynamic language that can do both simple and very complex things.','Python'),('The language that makes up the web.','HTML, CSS, JS')], validators=[DataRequired()])
+	submit = SubmitField('Code!')
 @app.route('/')
 def index():
   return render_template("index.html", db=db)
 
 @app.route('/code')
 def code():
-	form = forms.chose_language()
+	session['logged_in'] = False
+	form = chose_language()
 	if session.get('logged_in') == False:
 		flash('You must be logged in to view this page!')
 		return render_template('index.html')
@@ -25,6 +31,6 @@ def code():
 
 @app.route('/login')
 def login():
-	form = forms.login()
+	form = login()
 	
 app.run('0.0.0.0')
